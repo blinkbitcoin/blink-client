f0cd43afb196ffe24651164af5c79039b6373f65
Juan P Lopez <dolcalmi@gmail.com>
2025-10-27 16:22:18 -0500

    fix: add stricter URI encode adhering to RFC 3986 (#24)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -153,7 +153,7 @@ export default [
       ],
 
       "new-parens": "error",
-      "newline-per-chained-call": "error",
+      "newline-per-chained-call": "off",
       "no-alert": "error",
       "no-array-constructor": "error",
       "no-await-in-loop": "error",
